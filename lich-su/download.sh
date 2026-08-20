804e44cb9151a27cf40f4cc9e1fc794648936694
Dung Le Xuan <dung.lex@homecredit.vn>
2026-08-20 12:26:44 +0700

    Add history

---
+++ lich-su/download.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Download monthly gold prices dataset
+curl 'https://raw.githubusercontent.com/datasets/gold-prices/refs/heads/main/data/monthly.csv' \
+  -H 'Accept: application/json, text/plain, */*' \
+  -H 'Accept-Language: en-US,en;q=0.9,cs;q=0.8' \
+  -H 'Connection: keep-alive' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Site: same-origin' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.0.0' \
+  -H 'sec-ch-ua: "Not;A=Brand";v="8", "Chromium";v="150", "Microsoft Edge";v="150"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' > monthly.csv
