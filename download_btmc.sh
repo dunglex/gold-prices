ae11b8c2ec1389a4a31560ac2e58462b5fcde519
Dung Le Xuan <dung.lex@homecredit.vn>
2026-08-25 15:31:31 +0700

    Debug btmc

--- download_btmc.sh
+++ download_btmc.sh
@@ -2,7 +2,20 @@
 
 cd "$(dirname "$0")/data"
 
-# Download prices from btmc.vn
+echo "Downloading https://btmc.vn/"
+curl --connect-timeout 5 -k -v 'https://btmc.vn/' \
+  -H 'accept-language: en-US,en;q=0.9' \
+  -H 'cache-control: no-cache' \
+  -H 'pragma: no-cache' \
+  -H 'priority: u=1, i' \
+  -H 'sec-ch-ua: "Not;A=Brand";v="8", "Chromium";v="150", "Google Chrome";v="150"' \
+  -H 'sec-ch-ua-mobile: ?0' \
+  -H 'sec-ch-ua-platform: "Windows"' \
+  -H 'sec-fetch-dest: empty' \
+  -H 'sec-fetch-mode: cors' \
+  -H 'sec-fetch-site: same-origin' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36' > prices_btmc.html
+
 echo "Downloading https://btmc.vn/Home/BGiaVang"
 curl --connect-timeout 5 -k 'https://btmc.vn/Home/BGiaVang' \
   -H 'accept-language: en-US,en;q=0.9' \
@@ -17,6 +30,7 @@ curl --connect-timeout 5 -k 'https://btmc.vn/Home/BGiaVang' \
   -H 'sec-fetch-mode: cors' \
   -H 'sec-fetch-site: same-origin' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36' > prices_btmc_vang.html
+
 echo "Downloading https://btmc.vn/Home/BGiaBac"
 curl --connect-timeout 5 -k 'https://btmc.vn/Home/BGiaBac' \
   -H 'accept-language: en-US,en;q=0.9' \
