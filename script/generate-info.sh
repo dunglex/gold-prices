1de544b8a0d75d99e2141cea912b1cf0d3c43407
Dung Le Xuan <dung.lex@homecredit.vn>
2026-08-28 13:55:06 +0700

    Remove mihong

--- script/generate-info.sh
+++ script/generate-info.sh
@@ -11,7 +11,6 @@ json_count() {
 jq -n \
   --arg dt "$(date -u +%Y-%m-%dT%H:%M:%SZ)" \
   --argjson doji "$(json_count prices-doji.json '.')" \
-  --argjson mihong "$(json_count prices-mihong.json '.')" \
   --argjson phuquy "$(json_count prices-phuquy.json '.data')" \
   --argjson baotinmanhhai "$(json_count prices-baotinmanhhai.json '.data.goldRates.items')" \
   --argjson baotinminhchau "$(json_count prices-baotinminhchau.json '.sources')" \
@@ -21,7 +20,6 @@ jq -n \
     sources: {
       goldprice: 2,
       doji: $doji,
-      mihong: $mihong,
       phuquy: $phuquy,
       baotinmanhhai: $baotinmanhhai,
       baotinminhchau: $baotinminhchau,
